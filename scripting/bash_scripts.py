78886b48dbb51776d123c2f97b690b1a7b9273e6
Dan Griffiths <dg4978@outlook.com>
2026-04-15 13:41:37 +0200

    initial folder stucture

---
+++ scripting/bash_scripts.py
@@ -0,0 +1,4 @@
+
+
+# settingset up a folder structure
+mkdir -p ~/programming/github/solo/{data-structures,algorithms,oop,design-patterns,cli-tools,scripting,apis,etl-pipelines,databases,testing,projects,my-website}
