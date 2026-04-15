d4dd263abac3efc6a9daa573614bb8c85619aea8
Dan Griffiths <dg4978@outlook.com>
2026-04-15 13:49:46 +0200

    added .gitkeep

--- scripting/bash_scripts.py
+++ scripting/bash_scripts.py
@@ -2,3 +2,16 @@
 
 # settingset up a folder structure
 mkdir -p ~/programming/github/solo/{data-structures,algorithms,oop,design-patterns,cli-tools,scripting,apis,etl-pipelines,databases,testing,projects,my-website}
+# git init in repo route dir
+cd ~/code/solo
+git init
+touch README.md
+
+# add README to each empty dir + .gitkeep file to any empty folders so git tracks them
+touch ~/code/solo/algorithms/.gitkeep
+
+# add a file to all folders in local dir
+for dir in */; do touch "$dir.gitkeep"; done
+    
+# or more explicit — find instead of glob, skips the current dir, and works with folder names which have spaces.
+find . -maxdepth 1 -type d ! -name '.' -exec touch {}/.gitkeep \;
